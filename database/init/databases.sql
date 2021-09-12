4a47c59cb5a762cf145aed9a37633629a12fd1a9
aaronsleepy <aaron@kmong.com>
2021-09-12 19:07:22 +0900

    task: 로컬 MySQL DB docker 설정 추가

---
+++ database/init/databases.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE IF NOT EXISTS `partner`;
+CREATE DATABASE IF NOT EXISTS `order`;
+CREATE DATABASE IF NOT EXISTS `gift`;
+CREATE DATABASE IF NOT EXISTS `notification`;
+
+# create root user and grant rights
+# GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
