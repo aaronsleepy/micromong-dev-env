7ea6bf7b16ef48acaa32b1f7cac46f2546d9261c
aaronsleepy <aaron@kmong.com>
2021-12-14 16:57:13 +0900

    add seller db for batch output

--- database/init/databases.sql
+++ database/init/databases.sql
@@ -5,6 +5,7 @@ CREATE DATABASE IF NOT EXISTS `notification`;
 
 # hello-spring-batch 
 CREATE DATABASE IF NOT EXISTS `customer`;
+CREATE DATABASE IF NOT EXISTS `seller`;
 
 # create root user and grant rights
 # GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
